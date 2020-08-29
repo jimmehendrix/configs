8b268cac0a28ff581dbbc8da9bf19f99457ad849
Jimme Hendrix <hendrixjimme@gmail.com>
2020-08-29 16:44:24 +0200

    Initial

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const inquirer = require('inquirer');
+const path = require('path');
+const { readdir, writeFile, readFile } = require('fs').promises;
+
+const configFolderPath = path.resolve(__dirname, 'configs');
+
+const configFiles = {};
+
+(async () => {
+    const files = await readdir(configFolderPath).catch(console.log);
+
+    for (let i of files) {
+        const fileName = i.split('.')[1];
+        configFiles[fileName] = path.join(configFolderPath, i)
+    }   
+
+
+    const { stack } = await inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Are you working on front-end or back-end?',
+            name: 'stack',
+            choices: ['Front-end', 'Back-end']
+        }
+    ]);
+
+    let configs = [];
+    if (stack === 'Front-end') {
+        const { framework } = await inquirer.prompt([
+            {
+                type: 'list',
+                message: "What framework are you using?",
+                name: 'framework',
+                choices: ['Vue', 'none']
+            }
+        ])
+        
+        const fw = framework.toLowerCase();
+        const files = await readdir(configFolderPath + '/' + fw);
+        files.forEach(file => {
+
+            configs.push(
+                {
+                    filename: file,
+                    path: configFolderPath + '/' + fw + '/' + file
+                }
+            )
+        })
+    }
+
+    configs.forEach(async config => {
+        const content = await readFile(config.path).catch(console.log);
+        writeFile(config.filename, JSON.stringify(content.toString(), null, 2));
+    })
+
+})();
