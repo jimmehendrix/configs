77113c2f7d95503c22c71d51f1f19285f52bd292
Jimme <hendrixjimme@gmail.com>
2021-04-15 10:26:59 +0200

    Update some configs

--- src/configs/frameworks/vue2/.eslintrc.js
+++ src/configs/frameworks/vue2/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     env: {
         node: true,
     },
-    extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+    extends: ['plugin:vue/recommended', 'eslint:recommended'],
     parserOptions: {
         parser: 'babel-eslint',
     },
@@ -26,66 +26,5 @@ module.exports = {
                 ignores: [],
             },
         ],
-        "vue/order-in-components": ["error", {
-            "order": [
-            "el",
-            "name",
-            "key",
-            "parent",
-            "functional",
-            ["delimiters", "comments"],
-            ["components", "directives", "filters"],
-            "extends",
-            "mixins",
-            ["provide", "inject"],
-            "ROUTER_GUARDS",
-            "layout",
-            "middleware",
-            "validate",
-            "scrollToTop",
-            "transition",
-            "loading",
-            "inheritAttrs",
-            "model",
-            ["props", "propsData"],
-            "emits",
-            "setup",
-            "asyncData",
-            "data",
-            "fetch",
-            "head",
-            "computed",
-            "watch",
-            "watchQuery",
-            "LIFECYCLE_HOOKS",
-            "methods",
-            ["template", "render"],
-            "renderError"
-            ]
-        }],
-        "vue/attributes-order": ["error", {
-            "order": [
-            "DEFINITION",
-            "LIST_RENDERING",
-            "CONDITIONALS",
-            "RENDER_MODIFIERS",
-            "GLOBAL",
-            "UNIQUE",
-            "TWO_WAY_BINDING",
-            "OTHER_DIRECTIVES",
-            "OTHER_ATTR",
-            "EVENTS",
-            "CONTENT"
-            ],
-            "alphabetical": false
-        }]
     },
-    overrides: [
-        {
-            files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
-            env: {
-                jest: true,
-            },
-        },
-    ],
 };
